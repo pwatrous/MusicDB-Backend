38635d628f792251ea6b6d3c3591a5537bbbd344
Ifteda Ahmed-Syed <33703032+ifteda@users.noreply.github.com>
2020-04-14 14:19:19 -0400

    handle duplication (hopefully)

--- db/Nodes.py
+++ db/Nodes.py
@@ -1,53 +1,84 @@
-from py2neo import Node
-
+from py2neo import Node, Graph, NodeMatcher
 from Relationships import AppearsOn, Has, Creates
+
+import auth
+
+
+graph = Graph("bolt://localhost:7687", user=auth.user, password=auth.password)
+
+
+def node_exists(label, node):
+    print(type(node))
+    matcher = NodeMatcher(graph)
+    match = matcher.match(label, id=node["id"]).first()
+    return match
+
+
+def main():
+
+    tx = graph.begin()  # creates a transaction
+
+    tracks = []
+    # TODO parse inputs from frontend and fill tracks
+
+    for track in tracks:
+        if node_exists("Track", track) is None:  # track does not already exist in graph
+            track.commit_to_graph(tx)  # FIXME add param track first?
+
+    tx.commit()  # commits the transaction
 
 
 class Track(Node):
     # TODO figure out how to incorporate features
 
-    type = "track"
+    type = "Track"
 
     def __init__(self):
         # TODO figure out what params to expect
         self.name = ""
         self.href = ""
         self.id = ""
-        self.uri = "spotify:" + self.type + ":" + self.id
+        self.uri = ""
         self.duration_ms = 0
         self.explicit = False
         self.is_local = False
         self.popularity = 0
         self.track_number = 1
+
+        # TODO track features
+
         self.albums = []  # TODO create Album objects and store here
         self.artists = []  # TODO create Artist objects and store here
 
     def as_graph_node(self):
         return Node(type, name=self.name, href=self.href, id=self.id, uri=self.uri, duration_ms=self.duration_ms,
                     explicit=self.explicit, is_local=self.is_local, popularity=self.popularity,
-                    track_number=self.track_number)
+                    track_number=self.track_number)  # TODO track features
 
     def commit_to_graph(self, tx):
-        # FIXME make sure duplicate node is not being added
         tx.create(self.as_graph_node())
+        tx.process()
+
         for artist in self.artists:  # create Relationship: Artist CREATES Track
-            artist.commit_to_graph(tx)  # FIXME add param artist first?
+            if node_exists("Artist", artist) is None:
+                artist.commit_to_graph(tx)  # FIXME add param artist first?
             tx.create(Creates(artist, self).as_graph_edge())
         for album in self.albums:  # create Relationship: Track APPEARS_ON Album
-            album.commit_to_graph(tx)  # FIXME add param album first?
+            if node_exists("Album", album) is None:
+                album.commit_to_graph(tx)  # FIXME add param album first?
             tx.create(AppearsOn(self, album).as_graph_edge())
 
 
 class Album(Node):
 
-    type = "album"
+    type = "Album"
 
     def __init__(self):
         # TODO figure out what params to expect
         self.name = ""
         self.href = ""
         self.id = ""
-        self.uri = "spotify:" + self.type + ":" + self.id
+        self.uri = ""
         self.album_type = ""
         self.release_date = ""  # yyyy-mm-dd
         self.release_date_precision = ""  # e.g., "day"
@@ -62,20 +93,23 @@ class Album(Node):
     def commit_to_graph(self, tx):
         # FIXME make sure duplicate node is not being added
         tx.create(self.as_graph_node())
-        for artist in self.artists: # create Relationship: Album HAS Artist
-            tx.create(Has(self, artist).as_graph_edge())
+        tx.proces()
+        
+        for artist in self.artists:  # create Relationship: Album HAS Artist
+            if node_exists("Artist", artist) is None:
+                tx.create(Has(self, artist).as_graph_edge())
 
 
 class Artist(Node):
 
-    type = "artist"
+    type = "Artist"
 
     def __init__(self):
         # TODO figure out what params to expect
         self.name = ""
         self.href = ""
         self.id = ""
-        self.uri = "spotify:" + self.type + ":" + self.id
+        self.uri = ""
 
     def as_graph_node(self):
         return Node(type, name=self.name, href=self.href, id=self.id, uri=self.uri)
@@ -83,3 +117,4 @@ class Artist(Node):
     def commit_to_graph(self, tx):
         # FIXME make sure duplicate node is not being added
         tx.create(self)
+        tx.process()
